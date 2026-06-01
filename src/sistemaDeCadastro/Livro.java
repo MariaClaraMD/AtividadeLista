1c5ad02eaba751e5c0b5071deb7cf3bb19446f05
MariaClaraMD <monteiro.dutra@academico.ifpb.edu.br>
2026-06-01 18:09:20 -0300

    Classe Livro

---
+++ src/sistemaDeCadastro/Livro.java
@@ -0,0 +1,25 @@
+package sistemaDeCadastro;
+
+public class Livro {
+	
+	private long id;
+	
+	private String titulo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
